0842b056fb4dc2f0e6c7bba470d203cccc48ab1f
Javier Nualart Lee <nualartlee@gmail.com>
2019-02-01 12:38:04 -0800

    Updated test
    On branch submodule
    Your branch is up to date with 'origin/submodule'.
    
    Changes committed:
    
    	modified:   deploy-scripts
    	modified:   scripts/build.sh
    	modified:   scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
+# Work from projects's directory
+cd "${0%/*}/.."
 
-# Error check function
-# Param 1: return code, pass the last one with $?
-# Param 2: error text to display
-check_errs()
-{
-  if [ "${1}" -ne "0" ]; then
-    echo "ERROR # ${1} : ${2}"
-    exit ${1}
-  fi
-}
+# Import common functions
+source deploy-scripts/common.sh
 
 # Test that jenkins is responding on port 80
 echo "Requesting site..."
